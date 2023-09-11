e0a25433283c879b46795bafff71cb1057e4302b
Sistarp <sistarp720@gmail.com>
2023-09-11 10:28:17 -0300

    Atualização do estilo CSS para melhor legibilidade

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// Exemplo de dados dos profissionais (substitua com seus próprios dados)
 const professionalsData = [
   {
     nome: "Patricia Cabello",
@@ -30,7 +29,12 @@ const professionalsData = [
   {
     nome: "Andrea Campoi",
     cidade: "Santo André / SP",
-    especialidades: ["Especialidade 1", "Especialidade 2", "Especialidade 3"],
+    especialidades: [
+      "Organização Residencial",
+      "Pré e Pós Mudança",
+      "Recém Casados",
+      "Atendimento nas regiões: São Paulo, Grande ABC, Interior de SP"
+    ],
     foto: "campoi.jpeg",
     instagram: "https://www.instagram.com/andreacampoiorganizer/",
     whatsapp: "+55 (11) 95450-0076"
@@ -106,6 +110,17 @@ const professionalsData = [
     especialidades: [
       "Organização Residencial",
       "Mudança Organizada",
+    ],
+    foto: "quissack.jpg",
+    instagram: "https://www.instagram.com/claudia.quissack/",
+    whatsapp: "+55 (11) 97090-1565"
+  },
+    {
+    nome: "Valéria Cristina de Freitas",
+    cidade: "Santo André / SP",
+    especialidades: [
+      "Organização Residencial",
+      "Mudança Organizada",
       "Coordenação da Casa (Concierge)",
       "Consultoria de Organização e de Mudança",
       "Organização de Mala de Viagem",
@@ -115,12 +130,41 @@ const professionalsData = [
       "Palestras e Workshops",
       "Atendimento presencial em São Paulo e Região"
     ],
-    foto: "quissack.jpg",
-    instagram: "https://www.instagram.com/claudia.quissack/",
+    foto: "valeriafreitas.jpg",
+    instagram: "https://www.instagram.com/espaco_pratico/",
+    whatsapp: "+55 (11) 97090-1565"
+  },
+    {
+    nome: "Rosangela Kubota",
+    cidade: "São Caetano do Sul / SP",
+    especialidades: [
+      "Organização Residencial",
+      "Mudança Organizada",
+      "Coordenação da Casa (Concierge)",
+      "Consultoria de Organização e de Mudança",
+      "Organização de Mala de Viagem",
+      "Consultoria de Compra de Produtos Organizadores",
+      "Treinamento de Limpeza Moderna",
+      "Elaboração de Rotinas Domésticas",
+      "Palestras e Workshops",
+      "Atendimento presencial em São Paulo e Região"
+    ],
+    foto: "rokubota.jpg",
+    instagram: "https://www.instagram.com/ro_organiza_/",
     whatsapp: "+55 (11) 97090-1565"
   }
   // Adicione mais profissionais aqui
 ];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Chama a função para embaralhar o array antes de exibir os profissionais
+shuffleArray(professionalsData);
 
 // Função para exibir os profissionais na página
 function displayProfessionals() {
@@ -138,9 +182,29 @@ function displayProfessionals() {
     name.textContent = professional.nome;
 
     const city = document.createElement("p");
+    city.classList.add("city");
     city.textContent = `Cidade: ${professional.cidade}`;
 
-    const specialties = document.createElement("ul"); // Lista não ordenada
+    const arrowButton = document.createElement("button");
+    arrowButton.textContent = "Especialidades"; // Você pode usar um ícone de seta para baixo aqui
+    arrowButton.className = "arrow-button";
+
+    arrowButton.addEventListener("click", function () {
+      const specialtiesList = professionalCard.querySelector(
+        ".specialties-list"
+      );
+      if (
+        specialtiesList.style.display === "none" ||
+        specialtiesList.style.display === ""
+      ) {
+        specialtiesList.style.display = "block";
+      } else {
+        specialtiesList.style.display = "none";
+      }
+    });
+
+    const specialties = document.createElement("ul");
+    specialties.classList.add("specialties-list");
     professional.especialidades.forEach((especialidade) => {
       const listItem = document.createElement("li");
       listItem.textContent = especialidade;
@@ -161,13 +225,63 @@ function displayProfessionals() {
     professionalCard.appendChild(img);
     professionalCard.appendChild(name);
     professionalCard.appendChild(city);
+    professionalCard.appendChild(arrowButton);
     professionalCard.appendChild(specialties);
     professionalCard.appendChild(instagramButton);
     professionalCard.appendChild(whatsappButton);
-
+    specialties.classList.add("hidden");
     professionalsList.appendChild(professionalCard);
   });
 }
 
+function filterProfessionalsByCity(city) {
+  const professionalsList = document.getElementById("professionals-list");
+  const professionals = professionalsList.getElementsByClassName(
+    "professional-card"
+  );
+
+  for (const professional of professionals) {
+    const cityElement = professional.querySelector(".city");
+
+    if (city === "all" || cityElement.textContent.includes(city)) {
+      professional.style.display = "block"; // Exibe o profissional
+    } else {
+      professional.style.display = "none"; // Oculta o profissional
+    }
+  }
+}
+
+// Event listener para o elemento de seleção (dropdown)
+const cityFilter = document.getElementById("city-filter");
+cityFilter.addEventListener("change", function () {
+  const selectedCity = cityFilter.value;
+  filterProfessionalsByCity(selectedCity);
+});
+
 // Chama a função para exibir os profissionais quando a página carregar
 window.addEventListener("load", displayProfessionals);
+
+// JS do horário e data
+function atualizarDataHora() {
+  var elementoDataHora = document.getElementById("data-hora");
+  var dataHoraAtual = new Date();
+
+  var opcoesData = {
+    month: "numeric",
+    day: "numeric"
+  };
+
+  var opcoesHora = {
+    hour: "2-digit",
+    minute: "2-digit"
+  };
+
+  var dataFormatada = dataHoraAtual.toLocaleDateString(undefined, opcoesData);
+  var horaFormatada = dataHoraAtual.toLocaleTimeString(undefined, opcoesHora);
+
+  elementoDataHora.innerHTML = dataFormatada + " " + horaFormatada;
+}
+
+atualizarDataHora();
+
+setInterval(atualizarDataHora, 1000);
